d62d107655c81742509ed6f8be429e2b60c92ff5
Nick Walker <nick.walker@puppetlabs.com>
2017-01-20 08:32:24 -0800

    Fix cache_facts_autofill example

--- examples/cache_facts_autofill_for_60_mins.pp
+++ examples/cache_facts_autofill_for_60_mins.pp
@@ -2,9 +2,8 @@ service { 'pe-nginx' :
   ensure => 'running',
 }
 
-pe_nginx_cache::proxy_cache_path { 'my_cache' : }
+include pe_nginx_cache::proxy_cache_path
 
 pe_nginx_cache::cache_endpoint { '/api/cm/facts' :
-  endpoint          => '/api/cm/facts',
   proxy_cache_valid => '200 60m'
 }
